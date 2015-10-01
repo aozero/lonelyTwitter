788c9cad29a96e7dafcf74990637c717774c2dee
aozero <ozero@ualberta.ca>
2015-09-30 18:53:30 -0600

    Implemented addTweet()'s duplicate tweet test; lab exercise time is ending soon

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -7,6 +7,7 @@ import android.test.ActivityInstrumentationTestCase2;
  * Created by ozero on 9/30/15.
  */
 public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
     public TweetListTest() {
         super(LonelyTwitterActivity.class);
     }
@@ -32,19 +33,40 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
         Tweet tweet = new NormalTweet("HI");
         tweetList.add(tweet);
         assertTrue(tweetList.hasTweet(tweet));
+        boolean tweetDuplicateFlag = false;
+        try{
+            if(tweetList.hasTweet(tweet)) {
+                throw new IllegalArgumentException();
+            } else {
+                tweetList.add(tweet);
+            }
+            tweetList.add(tweet);
+        } catch (IllegalArgumentException e) {
+            tweetDuplicateFlag = true;
+        }
+        assertTrue(tweetDuplicateFlag);
     }
 
-    public void testTweetCount() {
+    public void testGetCount() {
 
     }
 
     public void testGetTweet() {
         TweetList tweetList = new TweetList();
         Tweet tweet = new NormalTweet("HI");
+        Tweet tweet2 = new NormalTweet("HI2");
+        Tweet tweet3 = new NormalTweet("HI3");
+
         tweetList.add(tweet);
         Tweet returnedTweet = tweetList.getTweet(0);
-        assertTrue((tweet.date.equals(returnedTweet.date)) && 
+        assertTrue((tweet.date.equals(returnedTweet.date)) &&
                 tweet.getText().equals(returnedTweet.getText()));
+
+        tweetList.add(tweet2);
+        tweetList.add(tweet3);
+
+
+
     }
 
     public void testGetTweetType() {
